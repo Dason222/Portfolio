f663528a8abc48815cbd0695a482f6c50a181f02
Dason222 <dason222@gmail.com>
2023-04-04 12:22:03 +0300

    Update Clients-card.vue

--- components/Clients-card.vue
+++ components/Clients-card.vue
@@ -3,7 +3,7 @@
     <div class="px-20 grid grid-rows-1 grid-flow-col gap-8 pr-10 py-8 bg-gradient-to-b from-gray-lightest to-gray-lightest2">
 
       
-        <div class=" relative h-96 w-full bg-cover object-cover " title="Mountain">
+        <div class=" relative h-96 w-full bg-cover object-cover " title="Morflax">
 
           <nuxt-link to="/morflax" class="inline-flex items-center justify-center text-lg md:text-2xl lg:text-3xl font-medium text-black">
               <img class="shadow-2xl	 rounded-3xl object-contain h-96 w-full object-cover" src="img/thubnail(hard-surface).png" >
@@ -15,7 +15,7 @@
         </div>
 
      
-         <div class="relative h-96 w-full bg-cover object-cover "  title="Mountain">
+         <div class="relative h-96 w-full bg-cover object-cover "  title="Strato homes">
           
           <nuxt-link to="/morflax" class="inline-flex items-center justify-center text-lg md:text-2xl lg:text-3xl font-medium text-black">
               <img class="shadow-2xl rounded-3xl object-contain h-96 w-full object-cover" src="img/hoise.png" >
@@ -25,14 +25,14 @@
           <h2 class="px-7 pb-2 absolute text-lg bottom-2  text-white  ">Creative technology company, building tools for the web and metaverse </h2>
          </div>
 
-          <div class="relative rounded-3xl w-auto h-full bg-cover "  title="Mountain">
+          <div class="relative rounded-3xl w-auto h-full bg-cover "  title="3D Clothing">
             <button> <img class="shadow-2xl	 rounded-3xl object-contain h-96 w-full object-cover " src="img/thubnail(hard-surface).png" ></button>
             <h3 class="absolute text-2xl font-medium text-white top-5 left-7">3D Clothing</h3>
             <h2 class="px-7 pb-2 absolute text-lg bottom-2  text-white  ">Creative technology company, building tools for the web and metaverse </h2>
         </div>
 
         <!--Card 3-->
-        <div class="relative rounded-3xl w-auto h-full bg-cover "  title="Mountain">
+        <div class="relative rounded-3xl w-auto h-full bg-cover "  title="3D Visualization">
           <button> <img class="shadow-2xl	 rounded-3xl object-contain h-96 w-full object-cover" src="img/thubnail(hard-surface).png" ></button>
             <h3 class="absolute text-2xl font-medium text-white top-5 left-7">3D Visualization</h3>
             <h2 class="px-7 pb-2 absolute text-lg bottom-2  text-white  ">Creative technology company, building tools for the web and metaverse </h2>
